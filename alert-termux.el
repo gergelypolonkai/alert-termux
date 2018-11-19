225fad8e24900ce3f73697133def84a941e49dc8
Gergely Polonkai <gergely@polonkai.eu>
2018-11-19 10:31:25 +0100

    Add copyright notice to alert-termux.el

--- alert-termux.el
+++ alert-termux.el
@@ -1,5 +1,25 @@
 ;;; alert-termux --- alert.el notifications on Termux
 
+;; Copyright (C) 2018 Gergely Polonkai
+
+;; Author: Gergely Polonkai <gergely@polonkai.eu>
+;; Keywords: termux, alert, notification
+;; Version: 1.0
+;; Package-Requires: ((emacs "24.4"))
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program. If not, see <http://www.gnu.org/licenses/>.
+
 ;;; Commentary:
 ;;
 ;; Alert style that uses termux-notification to send alerts.
